08f80d8271da55255bdbd8dba3f9a327d0c8a422
jehad-halahla <88844444+jehad-halahla@users.noreply.github.com>
2023-05-05 06:24:24 +0300

    Committing files

---
+++ CodeForces/Java 8/1325A | EhAb AnD gCd/152847604.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+   int t = input.nextInt();
+   for(int i = 0 ; i < t ; i++){
+     int n = input.nextInt();
+     System.out.println((n-1) + " " + 1);
+    }
+  }
+}
