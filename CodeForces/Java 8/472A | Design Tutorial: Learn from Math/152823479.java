520b37b7a9bc13ef6b211b22c0de00227dfecf60
jehad-halahla <88844444+jehad-halahla@users.noreply.github.com>
2023-05-05 06:24:44 +0300

    Committing files

---
+++ CodeForces/Java 8/472A | Design Tutorial: Learn from Math/152823479.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    int x,y;
+    if(n%2 != 0 ){
+      System.out.printf("%d %d", (n-9) ,9);
+      
+    }
+  else{
+         System.out.printf("%d %d", (n-8) ,8);
+ 
+  }
+    
+    
+    
+  }
+}
