a1261d71afec20e81fba1a70b60fd76be7c3662e
jehad-halahla <88844444+jehad-halahla@users.noreply.github.com>
2023-05-05 06:22:18 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1717A | Madoka and Strange Thoughts/170658497.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t ;
+    cin >> t;
+    while(t--){
+        long long n ;
+        cin >> n;
+        cout << n + 2*(n/2) + 2*(n/3) << "\n";
+ 
+    }
+     return 0;
+}
