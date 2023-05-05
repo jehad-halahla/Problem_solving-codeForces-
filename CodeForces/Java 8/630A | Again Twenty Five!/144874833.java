a025ebd46bb9a0c54427e3942fc506bec91995ee
jehad-halahla <88844444+jehad-halahla@users.noreply.github.com>
2023-05-05 06:26:34 +0300

    Committing files

---
+++ CodeForces/Java 8/630A | Again Twenty Five!/144874833.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+ public static void main (String [] args){
+  Scanner input = new Scanner(System.in);
+  String x = input.next();
+  System.out.println(25);
+ }
+}
