537699f491eb5559b1f0bb7300f2237960ed8ac2
jehad-halahla <88844444+jehad-halahla@users.noreply.github.com>
2023-05-05 06:25:12 +0300

    Committing files

---
+++ CodeForces/Java 8/959A | Mahmoud and Ehab and the even-odd game/146251132.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+ public static void main(String [] args){
+  Scanner input = new Scanner(System.in);
+  int n = input.nextInt();
+  if(n%2 != 0 )
+   System.out.println("Ehab");
+  else
+   System.out.println("Mahmoud");
+ 
+ }
+}
