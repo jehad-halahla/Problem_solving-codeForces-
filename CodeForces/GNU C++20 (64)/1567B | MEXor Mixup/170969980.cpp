dd36fefcc95c594c2497e698ab93f50f62c2e27a
jehad-halahla <88844444+jehad-halahla@users.noreply.github.com>
2023-05-05 06:21:35 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1567B | MEXor Mixup/170969980.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b;
+//known theorem
+        cin >> a >> b;
+        int xorOfA;
+        if(a%4 == 1)xorOfA = a-1;
+        else if(a%4 == 2)xorOfA = 1;
+        else if(a%4 == 3)xorOfA = a;
+        else xorOfA = 0;
+        if(xorOfA == b)cout << a << "\n";
+        else if((xorOfA^b) != a )cout << a+1 << "\n";
+        else cout << a+2 << "\n";
+    }
+ 
+    return 0;
+}
+ 
+ 
+ 
